afcd61002bc4a087e4c8e946b9707cd0b6dc7720
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-11 10:48:48 +0200

    several improvements

--- React/src/components/FoundImages.js
+++ React/src/components/FoundImages.js
@@ -5,6 +5,7 @@ class FoundImages extends Component {
         return (
             <div className="found-images">
                 {
+                    !this.props.images[0] ? <h1>No images found</h1> :
                     this.props.images.map((image, index) => {
                         return <img key={index} src={image.image} alt={image.title} />
                     })
--- vanilla-js/script.js
+++ vanilla-js/script.js
@@ -26,19 +26,22 @@ async function onSubmit() {
 function displayImages(images) {
     console.log(imageContainer);
     
+    images[0] ?
     images.forEach(image => {
         const img = document.createElement('img');
         img.src = image.image;
         imageContainer.append(img)
-    })
+    }) : imageContainer.innerHTML = "<h1>No images Found</h1>"
     loading = false;
     checkLoading()
 }
 
 function checkLoading() {
     if (loading) {
+        imageContainer.style.display = "none"
         document.getElementById("loader").style.display = "block"
     } else {
+        imageContainer.style.display = "grid"
         document.getElementById("loader").style.display = "none"
     }
 }
